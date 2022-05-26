df8386be9e21d5e7ac8879030ff460a25454906a
ShiraOhana <soshiraohana@gmail.com>
2022-05-26 21:11:05 +0300

    data

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img src="https://images.unsplash.com/photo-1610650499378-34d5aaf719d6?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170"></img>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/data.js
@@ -0,0 +1,22 @@
+data = [
+  {
+    city: "Rome",
+    img: "https://images.unsplash.com/photo-1552832230-c0197dd311b5?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1096",
+  },
+  {
+    city: "Paris",
+    img: "https://images.unsplash.com/photo-1431274172761-fca41d930114?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170",
+  },
+  {
+    city: "Netherland",
+    img: "https://images.unsplash.com/photo-1605704320412-5c3255bf47a9?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170",
+  },
+  {
+    city: "London",
+    img: "https://images.unsplash.com/photo-1520986606214-8b456906c813?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170",
+  },
+  {
+    city: "Lapland",
+    img: "https://images.unsplash.com/photo-1610650499378-34d5aaf719d6?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170",
+  },
+];
